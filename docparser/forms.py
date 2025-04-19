5918fec5e3976d6178e3b1b9a9231eaee6ed01b5
AKpal-codes <pal.ankur.mail@gmail.com>
2025-04-19 19:49:42 +0530

    fixed footer

--- docparser/forms.py
+++ docparser/forms.py
@@ -2,8 +2,7 @@ from django import forms
 
 class DocumentUploadForm(forms.Form):
     file = forms.FileField(
-        label='Select a file',
-        help_text='max. 2.5 MB',
+        label='Select a Business Document',
         widget=forms.ClearableFileInput(attrs={
             'class': 'form-control',
             'placeholder': 'Choose a file'
